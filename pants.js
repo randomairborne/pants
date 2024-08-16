ae5194baaa3813a4c1d1304a0eb54a3aa7e532e3
valkyrie_pilot <valk@randomairborne.dev>
2024-08-16 11:00:30 -0600

    Initial commit

---
+++ pants.js
@@ -0,0 +1,15 @@
+let reloaded = false;
+function changeIntoPants() {
+    const pathname = window.location.pathname;
+    if (pathname.includes("/shorts/") && !reloaded) {
+        const [[_, videocode]] = pathname.matchAll(/\/shorts\/(.*)/g);
+        const destination = `https://www.youtube.com/watch?v=${videocode}`;
+        console.debug(`PANTS: pathname: "${pathname}" video code: "${videocode}" destination: "${destination}"`);
+        reloaded = true;
+        window.location = destination;
+    } else if (!pathname.includes("/shorts/") && reloaded) {
+        reloaded = false;
+    }
+}
+
+setInterval(changeIntoPants, 50)
